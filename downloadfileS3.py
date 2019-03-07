f82b28977bccdb8c7da57ac8764269fd0ea09b9c
root <root@ip-172-31-31-62.us-east-2.compute.internal>
2019-03-07 13:03:05 +0000

    latest code changes for s3

--- downloadfileS3.py
+++ downloadfileS3.py
@@ -1,20 +1,28 @@
 import boto3
 import botocore
 
-BUCKET_NAME = 'rajashekar-smartshift' # replace with your bucket name
-KEY = 'raju.txt' # replace with your object key
+BUCKET_NAME = 'datagrokrassessment' # bucket name
+KEY = 'task.txt' # File in bucket
 
 s3 = boto3.resource('s3')
 
 try:
-    s3.Bucket('rajashekar-smartshift').download_file(KEY,'raju1.txt')
+    s3.Bucket('datagrokrassessment').download_file(KEY,'task.txt')
 except botocore.exceptions.ClientError as e:
     if e.response['Error']['Code'] == "404":
         print("The object does not exist.")
     else:
         raise
 
-data = open('raju1.txt').read()
-count = data.count('Devops')
+from string import punctuation
+N = 10
+words = {}
+words_gen = (word.strip(punctuation).lower() for line in open("task.txt")
+for word in line.split())
+for word in words_gen:
+    words[word] = words.get(word, 0) + 1
+    top_words = sorted(words.iteritems(),
+    key=lambda(word, count): (-count, word))[:N]
+for word, frequency in top_words:
+    print "%s: %d" % (word, frequency)
 
-print count
